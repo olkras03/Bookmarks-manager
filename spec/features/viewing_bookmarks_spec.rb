180447752bf9cf28b3b2c79b191ee8fe705b9108
Joe Ball <joe.ball@bglgroup.co.uk>
2019-11-04 14:49:45 +0000

    failing /bookmarks test

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -4,4 +4,9 @@ feature "Viewing bookmarks" do
     visit("/")
     expect(page).to have_content "Bookmark Manager"
   end
+
+  scenario "viewing bookmarks at /bookmarks" do
+    visit ("/bookmarks")
+    expect(page).to have_content "Bookmarks:"
+  end
 end
